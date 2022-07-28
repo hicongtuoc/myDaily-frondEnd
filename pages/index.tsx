a27efb168ffe9436aad0b08126db690428e3cba7
Quan Long <nattru0101@gmail.com>
2022-07-28 23:15:50 +0700

    Liohufg

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! Long</a>
+          Welcome to <a href="https://nextjs.org">Next.js! Long long</a>
         </h1>
 
         <p className={styles.description}>
