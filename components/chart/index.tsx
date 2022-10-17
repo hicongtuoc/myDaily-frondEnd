0486813abd59d55088cf19fccd62fe3b3c16a603
Long Lê Đăng <hicongtuoc@gmail.com>
2022-10-18 01:07:46 +0700

    add reCharts

--- components/chart/index.tsx
+++ components/chart/index.tsx
@@ -1,63 +1,77 @@
 import "./index.scss";
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import { Column } from '@ant-design/plots';
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 export default function ChartData() {
 
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    asyncFetch();
-  }, []);
-
-  const asyncFetch = () => {
-    fetch('https://gw.alipayobjects.com/os/antfincdn/iPY8JFnxdb/dodge-padding.json')
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => {
-        console.log('fetch data failed', error);
-      });
-  };
-  const config = {
-    data: data,
-    isGroup: true,
-    xField: '月份',
-    yField: '月均降雨量',
-    seriesField: 'name',
-    dodgePadding: 2,
-    label: {
-      position: 'middle',
-      layout: [
-        {
-          type: 'interval-adjust-position',
-        },
-        {
-          type: 'interval-hide-overlap',
-        },
-        {
-          type: 'adjust-color',
-        },
-      ],
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
     },
-  };
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
 
-  const LabelPlot = {
-    position: 'middle',
-    layout: [
-      {
-        type: 'interval-adjust-position',
-      },
-      {
-        type: 'interval-hide-overlap',
-      },
-      {
-        type: 'adjust-color',
-      },
-    ],
-  };
-
-  return <div>
-    <Column data={data} xField={"Date"} yField={"Time"}  seriesField={'name'} dodgePadding={2} />
-  </div>;
+  return <div className="w-10/12">
+    <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
+  </div>
+  
 }
