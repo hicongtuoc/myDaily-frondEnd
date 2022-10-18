dc96f2ce45c421737591ff4896ffa0594212390f
Long Lê Đăng <hicongtuoc@gmail.com>
2022-10-19 00:35:59 +0700

    Update index.tsx

--- components/sensor/control-sensor/index.tsx
+++ components/sensor/control-sensor/index.tsx
@@ -1,9 +1,19 @@
-import {Button, Drawer} from "antd";
+import {Button, Drawer, notification, Switch} from "antd";
 import {useState} from "react";
 import 'antd/dist/antd.css';
 
 export default function ControlSensor() {
   const [open, setOpen] = useState(false);
+
+  type NotificationType = "success" | "info" | "warning" | "error";
+  
+  const openNotificationWithIcon = (type: NotificationType , isOpen: boolean) => {
+    notification[type]({
+      message: "Trạng thái",
+      description: `${isOpen}`,
+    });
+  };
+
 
   const showDrawer = () => {
     setOpen(true);
@@ -13,15 +23,29 @@ export default function ControlSensor() {
     setOpen(false);
   };
 
+  const onChange = (checked: boolean) => {
+    console.log(`switch to ${checked}`);
+    openNotificationWithIcon('info', checked)
+  };
+
   return (
     <div className="">
       <Button type="primary" onClick={showDrawer}>
         Cotrol Sensor
       </Button>
       <Drawer title="Cotrol Sensor" placement="right" onClose={onClose} visible={open}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <div className="my-3">
+          <span className="mr-4">Máy bơm</span>
+          <Switch defaultChecked onChange={onChange} />
+        </div>
+        <div className="my-3">
+          <span className="mr-4">Đèn</span>
+          <Switch defaultChecked onChange={onChange} />
+        </div>
+        <div className="my-3">
+          <span className="mr-4">Quạt</span>
+          <Switch defaultChecked onChange={onChange} />
+        </div>
       </Drawer>
     </div>
   );
